eae19772fb7275121fa601d1cba301ebb57d84aa
Maverick S <maverickmmsc@gmail.com>
2024-11-17 13:39:12 -0500

    nav bar update

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <div className="nav" style={{wordSpacing: "1px", justifyContent:"space-evenly", width: "20%" }}>
+      <Link to="/">Search Candidates</Link>
+      <Link to="/SavedCandidates">Saved Candidates</Link>
+    </div>
   )
 };
 
